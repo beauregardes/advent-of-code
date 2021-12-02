e6a89fa24bc38d3f8b7816bdaba1e68855880748
Bo Fehlan <bo.fehlan@gmail.com>
2021-12-02 00:26:03 -0700

    problems 1 and 2

---
+++ 2021/1/main.py
@@ -0,0 +1,25 @@
+def calc(measurements, window_size):
+  ans = 0
+  for i in range(len(measurements) - window_size):
+    w1 = measurements[i:i+window_size]
+    w2 = measurements[i+1:i+1+window_size]
+    if sum(map(int, w1)) < sum(map(int, w2)):
+      ans += 1
+  return ans
+
+
+def main():
+  with open('2021/1/input.txt', 'r') as f:
+    measurements = f.readlines()
+
+  p1_ans = calc(measurements, 1)
+  p2_ans = calc(measurements, 3)
+
+  return p1_ans, p2_ans
+
+
+if __name__ == '__main__':
+  p1_ans, p2_ans = main()
+
+  print(f"P1: {p1_ans}")
+  print(f"P2: {p2_ans}")
